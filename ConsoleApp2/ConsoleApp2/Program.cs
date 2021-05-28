b7d258765ecfd145d151578451fefcbf5dc6a5b1
ANARSALE <anjalinarsale88@gmail.com>
2021-05-28 16:46:57 +0530

    first commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[,] arr = new int[2, 2];
+			int[,] trans = new int[2, 2];
+			int row, col;
+			Console.WriteLine("Enter Values into Array: ");
+			for (row = 0; row < 2; row++)
+			{
+				for (col = 0; col < 2; col++)
+				{
+					arr[row, col] = Convert.ToInt32(Console.ReadLine());
+				}
+			}
+
+			for (row = 0; row < 2; row++)
+			{
+				for (col = 0; col < 2; col++)
+				{
+					trans[col, row] = arr[row, col];
+				}
+			}
+
+			Console.WriteLine("Actual array");
+			for (row = 0; row < 2; row++)
+			{
+				for (col = 0; col < 2; col++)
+				{
+					Console.Write(arr[row, col] + "\t");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine("Transpose array");
+			for (row = 0; row < 2; row++)
+			{
+				for (col = 0; col < 2; col++)
+				{
+					Console.Write(trans[row, col] + "\t");
+				}
+				Console.WriteLine();
+			}
+			}
+		}
+}
